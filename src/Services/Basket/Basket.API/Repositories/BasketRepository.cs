29d96d907b3c2828d6d6523cdcb7c2c479d503e9
Petreo <petr.kholodniuk@gmail.com>
2023-01-07 14:12:18 +0200

    Add Basket.API service

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,39 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCashe;
+        public BasketRepository(IDistributedCache redisCashe)
+        {
+            _redisCashe = redisCashe ?? throw new ArgumentNullException(nameof(redisCashe));
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCashe.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCashe.GetStringAsync(userName);
+            if (String.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCashe.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
